478d2406ae856f5854718c3435a5ca7326d5d831
RioChx <73mahy@gmail.com>
2025-12-24 20:06:05 +1100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
+// ROOT build.gradle.kts
 plugins {
+    // This defines the version for the whole project
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
