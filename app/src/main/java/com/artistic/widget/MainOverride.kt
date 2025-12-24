0b8f0e7dbbcb94be3de8fa7dbf5993591ea52e3d
RioChx <73mahy@gmail.com>
2025-12-24 23:52:29 +1100

    Update MainOverride.kt

--- app/src/main/java/com/artistic/widget/MainOverride.kt
+++ app/src/main/java/com/artistic/widget/MainOverride.kt
@@ -5,23 +5,19 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 
 object MainOverride {
-    // Canvas & Container
     var canvasBgColor by mutableStateOf(Color(0xFF121A16))
     var canvasOpacity by mutableStateOf(0.9f)
-    
-    // LED Engine (Animations: Scroll, Pulse, Blink, Rainbow, Glow Pulse, Bounce)
+
+    // LED Engine Settings
     var ledText by mutableStateOf("WIDGET PRO ARTISTIC")
     var ledColor by mutableStateOf(Color(0xFF00FF00))
     var ledFontSize by mutableStateOf(24f)
     var ledAnimSpeed by mutableStateOf(5) // 1-10 Slider
     var ledOffset by mutableStateOf(Offset(20f, 40f))
+    
+    // Animation Mode: "Rainbow", "Pulse", "Blink", or "Static"
+    var ledAnimationMode by mutableStateOf("Rainbow") 
 
-    // 12h Digital Clock
-    var clockColor by mutableStateOf(Color.White)
+    var showClock by mutableStateOf(true)
     var clockOffset by mutableStateOf(Offset(80f, 100f))
-
-    // Date (DD MMM YYYY)
-    var dateText by mutableStateOf("24 DEC 2025")
-    var dateColor by mutableStateOf(Color.LightGray)
-    var dateOffset by mutableStateOf(Offset(100f, 180f))
 }
