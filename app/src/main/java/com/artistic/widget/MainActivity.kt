0af2e21da1a6203bbcbf1f6c8ddcaadda9c54869
RioChx <73mahy@gmail.com>
2025-12-24 21:42:11 +1100

    Update MainActivity.kt

--- app/src/main/java/com/artistic/widget/MainActivity.kt
+++ app/src/main/java/com/artistic/widget/MainActivity.kt
@@ -4,29 +4,45 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // Background Box to ensure we aren't seeing black-on-black
             Box(modifier = Modifier
                 .fillMaxSize()
-                .background(Color.DarkGray)) {
+                .background(Color.Black)) {
                 
                 Text(
-                    text = "Widget Control Active",
-                    color = Color.White,
-                    fontSize = 24.sp,
-                    modifier = Modifier.padding(16.dp)
+                    text = MainControlRegistry.ledText,
+                    color = Color.Cyan,
+                    fontSize = 30.sp,
+                    modifier = Modifier
+                        .offset { 
+                            IntOffset(
+                                MainControlRegistry.clockX.roundToInt(), 
+                                MainControlRegistry.clockY.roundToInt()
+                            ) 
+                        }
+                        .pointerInput(Unit) {
+                            detectDragGestures { change, dragAmount ->
+                                change.consume()
+                                // The Hook: Updating the Main Override
+                                MainControlRegistry.clockX += dragAmount.x
+                                MainControlRegistry.clockY += dragAmount.y
+                            }
+                        }
                 )
-                
-                // Your Drag-and-Drop logic goes here
             }
         }
     }
