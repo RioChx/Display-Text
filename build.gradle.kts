ae074151c2ce301ed80295d7d788094a309c343c
RioChx <73mahy@gmail.com>
2025-12-25 00:29:42 +1100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
-// ROOT build.gradle.kts
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    // This defines the version for the whole project
     id("com.android.application") version "8.1.0" apply false
+    id("com.android.library") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 }
