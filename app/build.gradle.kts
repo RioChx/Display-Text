d53aff096fa326d2d4b2f5755bd2190bd245f5b4
RioChx <73mahy@gmail.com>
2025-12-24 19:46:45 +1100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,6 @@
-plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+android {
+    compileSdk = 33
+    defaultConfig {
+        targetSdk = 33
+    }
 }
