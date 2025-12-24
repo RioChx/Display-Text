f46148d2a24754bc09b8a08c7ec06de49cba067d
RioChx <73mahy@gmail.com>
2025-12-24 23:12:18 +1100

    Update ArtisticWidgetProvider.kt

--- app/src/main/java/com/artistic/widget/ArtisticWidgetProvider.kt
+++ app/src/main/java/com/artistic/widget/ArtisticWidgetProvider.kt
@@ -9,6 +9,8 @@ class ArtisticWidgetProvider : AppWidgetProvider() {
     override fun onUpdate(context: Context, manager: AppWidgetManager, ids: IntArray) {
         for (id in ids) {
             val views = RemoteViews(context.packageName, R.layout.widget_layout)
+            // Displays the text defined in your Main Override [cite: 2025-12-13]
+            views.setTextViewText(R.id.widget_text, MainControlRegistry.ledText)
             manager.updateAppWidget(id, views)
         }
     }
