e2bfdae8e11befd1330357ad9f4c84f208872510
RioChx <73mahy@gmail.com>
2025-12-24 23:13:59 +1100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,21 +15,16 @@ android {
         versionName = "1.0"
     }
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
     buildFeatures {
         compose = true
     }
 
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
+    }
+    
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
 
